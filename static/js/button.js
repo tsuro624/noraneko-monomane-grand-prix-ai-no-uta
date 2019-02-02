28c9a4981e404f46b13ea18281ce8d0c238036da
tsuro624 <mirukuke-ki@hotmail.co.jp>
2019-02-02 23:49:30 +0900

    実装

--- static/js/button.js
+++ static/js/button.js
@@ -4,16 +4,47 @@ var syncerSounds = {
 	currentTime: null ,
 } ;
 
-// 即時関数
+var koujo = {
+	"C-b": "ベイビー",
+	"C-r": "化膿してるHART",
+	"D-b": "プリーズ",
+	"D-r": "見せてよ",
+	"E-b": "メイク・ラブ",
+	"E-r": "スキルアップにつながる",
+	"F-b": "ヴァージン！ヴァージン！",
+	"F-r": "俺のキャパシティ",
+	"G-b": "ラブ・ミー",
+	"G-r": "君のインナースペース",
+	"A-b": "パラレルワールド",
+	"A-r": "ハン押してくれ",
+	"B-b": "バイト帰り",
+	"B-r": "飛びこんで来い",
+	"X": "僕はね、もう音楽なんてどうでもよくて、君のことが好きなんやけど、でも、あの、その、だから楽器を握るんじゃなくて、君の手を握りたいけど、だけれども、だけれでも、僕はもう、こうやって音楽を奏でて、君に言葉を伝えるその術しか持ってないから、僕は君の為に、歌う、も、ぼ、僕のために歌いたいんです",
+	"Cm-b": "俺のCマイナーコード",
+	"Cm-r": "それでもいいから",
+	"Dm-b": "セクシー・アマゾネス",
+	"Dm-r": "ハンパねえぜ",
+	"Em-b": "キス・ミー",
+	"Em-r": "俺の一問一答",
+	"Fm-b": "長いフェイドアウト",
+	"Fm-r": "メールがこない",
+	"Gm-b": "ヨイトマケ",
+	"Gm-r": "天候に恵まれない地方に生まれて",
+	"Am-b": "キャッチ・ミー・イフ・ユー・キャン",
+	"Am-r": "怖がらないで",
+	"Bm-b": "ウォンチュー",
+	"Bm-r": "君の名前のイレズミ",
+	"Y": "君が好き好き好っきゃねん ボコッ"
+};
+
 (function()
 {
 	// 設定
-	var setClass = 'sounds' ;							// ボタン要素のクラス名
-	var setDir = './' ;									// 音声ファイルがあるフォルダ(最後は[/])
-	var setStopButtonId = 'stop-button' ;			// 停止ボタンに付けるID
+	var soundsClass = 'sounds';	// ボタン要素のクラス名
+	var setDir = './sounds/'; // 音声ファイルがあるフォルダ(最後は[/])
 
 	// クラス名が付いた要素を取得する
-	var sounds = document.getElementsByClassName( setClass ) ;
+	var sounds = document.getElementsByClassName( soundsClass );
 
 	// 全ての要素にクリックイベントを設定する
 	for( var i=0,l=sounds.length ; l>i ; i++ )
@@ -23,57 +54,17 @@ var syncerSounds = {
 		{
 			// ファイル名の取得
 			var file = this.getAttribute( 'data-file' ) ;
+			 $('.main_image').attr('src', 'images/' + file + '.png');
+			 $('.koujo').text(koujo[file]);
 
-			// 一度生成したエレメントは生成しない
-			if( typeof( syncerSounds.flag[ file ] )=="undefined" || syncerSounds.flag[ file ] != 1 )
-			{
-				// 生成するエレメントの調整
-				var audio = document.createElement( 'audio' ) ;
+ 			// [audio]を生成するQ
+			var audio = document.createElement( 'audio' ) ;
+			audio.src = setDir + file + '.mp3' ;
+			document.body.appendChild( audio ) ;
 
-				// エレメントのIDを指定
-				audio.id = file ;
-				audio.src = setDir + file + '.mp3' ;
-
-				// [audio]を生成する
-				document.body.appendChild( audio ) ;
-			}
-
-			// 初回再生以外だったら音声ファイルを巻き戻す
-			stopCurrentSound() ;
-
-			// 音声ファイルを再生[play()]する
-			document.getElementById( file ).play() ;
-
-			// 最近再生した音声としてセット
-			syncerSounds.currentTime = file ;
-
-			// 初回再生が終わった判定用に[syncerSounds.flag]の値を0から1に変更する
-			// エレメントを何度も生成しないようにするため
-			syncerSounds.flag[ file ] = 1 ;
-
-			// 終了
+			// 再生
+			audio.play();
 			return false ;
 		}
 	}
-
-	// 前回の音声を停止する関数
-	function stopCurrentSound()
-	{
-		var currentSound = document.getElementById( syncerSounds.currentTime ) ;
-
-		if( currentSound != null )
-		{
-			currentSound.pause() ;
-			currentSound.currentTime = 0 ;
-		}
-	}
-
-
-	// 停止ボタンをクリックした時のイベント
-	document.getElementById( setStopButtonId ).onclick = function()
-	{
-		stopCurrentSound() ;
-		return false ;
-	}
-
 })() ;
